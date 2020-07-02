34e5552753b515731e67529d2b082727415cc6f9
laczkobalazs <laczko.balazs89@gmail.com>
2020-07-02 14:49:35 +0200

    adds env and sets up auth key

--- src/api/MovieContext.js
+++ src/api/MovieContext.js
@@ -7,22 +7,18 @@ export const MovieProvider = (props) => {
   const [movies, setMovies] = useState([]);
 
   const config = {
-    headers: { Authorization: `Bearer JFpvsjPAUwyKSPkb4PY1` },
+    headers: { Authorization: `Bearer REACT_API_KEY` },
   };
-
-  // const bodyParameters = {
-  //   key: "JFpvsjPAUwyKSPkb4PY1",
-  // };
 
   useEffect(() => {
     axios
       .get("https://the-one-api.herokuapp.com/v1/movie", config)
       .then((response) => {
-        setMovies(response.data);
+        setMovies(response.data.docs);
         console.log(movies);
       })
       .catch((err) => console.error(err));
-  }, []);
+  });
 
   return (
     <MovieContext.Provider value={movies}>
